516ac3906a76fc4929a4b5bc69b5ad2d1dba8dfd
zetno <ekruders@gmail.com>
2014-10-20 20:27:08 +0200

    test

---
+++ src/resource/UserResource.java
@@ -0,0 +1,52 @@
+package resource;
+
+import java.util.ArrayList;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import model.Model;
+import model.Movie;
+
+@Consumes("application/x-www-form-urlencoded")
+@Path("/user")
+public class UserResource {
+
+	@Context
+	ServletContext context;
+
+	private Model model;
+
+	private ArrayList<Movie> movies;
+
+	@GET
+	@Path("/movielist")
+	@Produces({ MediaType.APPLICATION_XML })
+	public ArrayList<Movie> getMovies(@HeaderParam("token") String token) {
+
+		model = (Model) context.getAttribute("Model");
+
+		movies = new ArrayList<Movie>();
+
+		// user has to be logged in for access
+		if (model.verifyWithToken(token)) {
+
+			for (Movie movie : model.getMovies()) {
+				model.getMovies();
+				movies.add(movie);
+			}
+			return movies;
+		} else {
+			return null;
+			// throw new WebApplicationException();
+		}
+	}
+}
